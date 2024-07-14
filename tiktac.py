e23d5bd8eeea67f0bb1552ff134760c2562a5b1f
sudipadk10 <adhikarisudip869@gmail.com>
2024-07-14 15:22:24 +0545

    Created a tictactoe game using py

--- tiktac.py
+++ tiktac.py
@@ -1,56 +1,107 @@
 
-def printBoard(xstate, zstate):
-    zero = 'X' if xstate[0] else ('O' if zstate[0] else 0)
-    one = 'X' if xstate[1] else ('O' if zstate[1] else 1)
-    two = 'X' if xstate[2] else ('O' if zstate[2] else 2)
-    three = 'X' if xstate[3] else ('O' if zstate[3] else 3)
-    four = 'X' if xstate[4] else ('O' if zstate[4] else 4)
-    five = 'X' if xstate[5] else ('O' if zstate[5] else 5)
-    six = 'X' if xstate[6] else ('O' if zstate[6] else 6)
-    seven = 'X' if xstate[7] else ('O' if zstate[7] else 7)
-    eight = 'X' if xstate[8] else ('O' if zstate[8] else 8)
-    
-    print(f"{zero} | {one} | {two}")
-    print(f"--|---|--")
-    print(f"{three} | {four} | {five}")
-    print(f"--|---|--")
-    print(f"{six} | {seven} | {eight}")
-
-def sum(a,b,c):
-    return a+b+c
-
-def checkwin(xstate, zstate):
-    wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],[0, 4, 8], [2, 4, 6]]
-    for win in wins:
-        if(sum(xstate[win[0]], xstate[win[1]], xstate[win[2]]) == 3):
-            print("X won")
-            
-             if(sum(zstate[win[0]], zstate[win[1]], zstate[win[2]]) == 3):
-            print("O won")
-            
-        else:
-            return -1
-   
-
-if __name__ == "__main__":
-    xstate= [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    zstate= [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    turn = 1 # 1 for X and 0 for O
-    print("Welcome to TIC TAC TOE")
-    printBoard(xstate,zstate)
+def main():
+    print("----TIk TAK TOE----")
+    print("Welcome to the Game \n")
+    p1 = input("Enter name of first player(X):")
+    p2 = input("Enter name of second player(O):")
+    turn =0
+    winner = 'unknown'
+    pos = [ '0','1','2','3','4','5','6','7','8']
+    def printboard():
+        print('Board :')
+        print( f'{pos[0]} | {pos[1]} | {pos[2]} ')
+        print('---------')
+        print( f'{pos[3]} | {pos[4]} | {pos[5]} ')
+        print('---------')
+        print( f'{pos[6]} | {pos[7]} | {pos[8]} ')
+    printboard()
     while(True):
-        if(turn==1):
-            print("X's Chance")
-            value = int(input("Please enter a value : "))
-            xstate[value] = 1
-            turn = 0
+        print("Enter the no of position you wanna mark ,")
+        if turn%2 ==0:
+            c1 = int(input(f"{p1}'s position : "))
+            pos[c1]='X'
+            
         else:
-            print("O's Chance")
-            value = int(input("Please enter a value : "))
-            zstate[value] = 1
-            turn =1      
-        printBoard(xstate,zstate)
-        
-        c= checkwin(xstate, zstate)
-        if c != -1:
+            c2 = int(input(f"{p2}'s position : "))
+            pos[c2]='O'
+        turn+=1
+        printboard()
+        if pos[0] == pos[1] and pos[1] == pos[2]:
+            if pos[0] == 'X':
+                winner = p1
+
+            elif pos[0] == 'O':
+                winner =p2
+        elif pos[3] == pos[4] and pos[4] == pos[5]:
+            if pos[3] == 'X':
+                winner = p1
+
+            elif pos[3] == 'O':
+                winner = p2
+        elif pos[6] == pos[7] and pos[7] == pos[8]:
+            if pos[6] == 'X':
+                winner = p1
+
+            elif pos[6] == 'O':
+                winner = p2
+
+        elif pos[0] == pos[3] and pos[3] == pos[6]:
+            if pos[0] == 'X':
+                winner = p1
+
+            elif pos[0] == 'O':
+                winner = p2
+
+        elif pos[1] == pos[4] and pos[4] == pos[7]:
+            if pos[1] == 'X':
+                winner = p1
+
+            elif pos[1] == 'O':
+                winner = p2
+        elif pos[2] == pos[5] and pos[5] == pos[8]:
+            if pos[2] == 'X':
+                winner = p1
+
+            elif pos[2] == 'O':
+                winner = p2
+        elif pos[0] == pos[4] and pos[4] == pos[8]:
+            if pos[0] == 'X':
+                winner = p1
+
+            elif pos[0] == 'O':
+                winner = p2
+
+        elif pos[6] == pos[4] and pos[4] == pos[2]:
+            if pos[6] == 'X':
+                winner = p1
+
+            elif pos[6] == 'O':
+                winner = p2
+
+        if winner!="unknown":
+            break
+        i=0
+        poscount=0
+        for p in pos:
+            if p != f'{i}':
+                poscount+=1
+            i+=1
+        if i == poscount :
+            print("it's a draw")
+            break
+
+    if winner!='unknown':
+        print(f'Congratulations !! {winner} won the game.')
+
+
+if __name__ == '__main__':
+    while(True):
+        main()
+        print("1.Restart 2.Quit")
+        choice = int(input("Enter your choice : "))
+        if choice == 1:
+            print("New Game !!\n")
+            continue
+        elif choice == 2:
+            print("Thanks for playing !")
             break
